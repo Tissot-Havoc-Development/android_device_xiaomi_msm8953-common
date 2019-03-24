36f83ff47d075439ea25e946d71f75c4e41a5c49
Mountaser Halak <mountaser1998@gmail.com>
2019-03-24 20:02:58 +0100

    msm8953-common: Disabled Dexpreopting Prebuilts
    Needed because of /Gcam

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      DONT_DEXPREOPT_PREBUILTS := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
     endif
